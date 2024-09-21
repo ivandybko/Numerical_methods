bc870d6cfde09037cdf44d570fba6519b864d8fe
Ivan Dybko <dyybkoie@student.bmstu.ru>
2024-09-21 21:00:59 +0300

    QR working

--- Lab 1/src/QR decomposition.h
+++ Lab 1/src/QR decomposition.h
@@ -31,9 +31,13 @@ void rotate(std::vector<std::vector<T>>& transit, std::vector<std::vector<T>>& A
 	T s = A[j][i]/std::sqrt(A[i][i]*A[i][i]+A[j][i]*A[j][i]);
 	size_t n = A.size();
 	for (int col = 0; col < n; ++col) {
-		T temp = transit[i][col];
-		transit[i][col] = c * transit[i][col] + s * transit[j][col];
-		transit[j][col] = (-s) * temp + c * transit[j][col];
+		T temp = c * A[i][col] + s * A[j][col];
+		A[j][col] = -s * A[i][col] + c * A[j][col];
+		A[i][col] = temp;
+		T temp1=c * transit[i][col] + s * transit[j][col];
+		T temp2=(-s) * transit[i][col] + c * transit[j][col];
+		transit[i][col] = temp1;
+		transit[j][col] = temp2;
 	}
 }
 
@@ -43,7 +47,6 @@ std::vector<T> QRdecompostition(const std::pair<std::vector<std::vector<T>>, std
 	auto b = data.second;
 	size_t n = A.size();
 	std::vector<std::vector<T>> transit(n,std::vector<T>(n,0));
-	std::vector<std::vector<T>> R(transit);
 	for (int i = 0; i < n; ++i)
 	{
 		transit[i][i]=1;
@@ -52,13 +55,60 @@ std::vector<T> QRdecompostition(const std::pair<std::vector<std::vector<T>>, std
 	{
 		for (int j = i+1; j < n; ++j)
 		{
-			if (A[j][i] != 0 or A[i][i] != 0){
+			if (A[j][i] != 0 or A[i][i]!=0){
 				rotate(transit,A,i,j);
 			}
 		}
 	}
-	multiply(transit,A,R);
+	std::vector<T> x(n);
+	std::vector<T> b1(n,0);
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			b1[i]+=transit[i][j]*b[j];
+		}
+	}
+	x[n-1]=b1[n-1]/A[n-1][n-1];
+	for (size_t i = n-1; i-- > 0; )
+	{
+		T sum{0};
+		for (size_t j = i; j < n; ++j)
+		{
+			sum+=A[i][j]*x[j];
+		}
+		x[i]=(b1[i]-sum)/A[i][i];
+	}
 	transpose(transit);
-	return std::vector<T>();
+//	std::vector<std::vector<T>> test(n,std::vector<T>(n,0));
+//	multiply(transit,A,test);
+//	std::cout << "Q:\n";
+//	for (auto row : transit)
+//	{
+//		for (auto element : row)
+//		{
+//			std::cout << element << ' ';
+//		}
+//		std::cout << '\n';
+//	}
+//	std::cout << "R:\n";
+//	for (auto row : A)
+//	{
+//		for (auto element : row)
+//		{
+//			std::cout << element << ' ';
+//		}
+//		std::cout << '\n';
+//	}
+//	std::cout << "QR:\n";
+//	for (auto row : test)
+//	{
+//		for (auto element : row)
+//		{
+//			std::cout << element << ' ';
+//		}
+//		std::cout << '\n';
+//	}
+	return x;
 }
 #endif //LAB_1_SRC_QR_DECOMPOSITION_H_
