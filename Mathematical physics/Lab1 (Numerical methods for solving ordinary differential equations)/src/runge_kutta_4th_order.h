2b7550fa2721691082d77dc71f6bdc53a6ebea22
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-14 11:18:31 +0300

    Minor fix

--- Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/runge_kutta_4th_order.h
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/runge_kutta_4th_order.h
@@ -3,21 +3,20 @@
 #define RUNGE_KUTTA_4TH_ORDER_CPP_H
 #include <functional>
 #include <vector>
-#include "../../../Linear algebra/Lab3 (Solving interpolation problems)/src/grid_generators.h"
+
 template<typename T>
 std::vector<std::vector<T>> RungeKutta4System(
 	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
 	T t0, const std::vector<T>& u0, double t_end, double tau){
 	size_t n = f.size();
 	size_t N =(t_end-t0)/tau+1;
-	std::vector<T> k1(n),k2(n),k3(n),k4(n),t(N);
-	for (size_t i = 0; i < N; i++) {
-		t[i]=t0+i*tau;
-	}
+	std::vector<T> k1(n),k2(n),k3(n),k4(n);
+	T t;
 	std::vector<std::vector<T>> solution(n,std::vector<T>(N));
 	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
 	for(int i = 0; i < N-1; i++)
 	{
+		t=t0+i*tau;
 		std::vector<T> u_n(n);
 		for(int j = 0; j < n; j++)
 		{
@@ -25,19 +24,19 @@ std::vector<std::vector<T>> RungeKutta4System(
 		}
 		for(int j = 0; j < n; j++)
 		{
-			k1[j]=f[j](t[i],u_n);
+			k1[j]=f[j](t,u_n);
 		}
 		for(int j = 0; j < n; j++)
 		{
-			k2[j]=f[j](t[i]+0.5*tau,u_n+0.5*tau*k1);
+			k2[j]=f[j](t+0.5*tau,u_n+0.5*tau*k1);
 		}
 		for(int j = 0; j < n; j++)
 		{
-			k3[j]=f[j](t[i]+0.5*tau,u_n+0.5*tau*k2);
+			k3[j]=f[j](t+0.5*tau,u_n+0.5*tau*k2);
 		}
 		for(int j = 0; j < n; j++)
 		{
-			k4[j]=f[j](t[i]+tau,u_n+tau*k3);
+			k4[j]=f[j](t+tau,u_n+tau*k3);
 		}
 		for(int j = 0; j < n; j++)
 		{
