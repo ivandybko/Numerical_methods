27e2ebbcb7679c3ae5de9e1441b95e2de0ef8c0f
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-14 11:18:58 +0300

    Impicit and Explicit Euler methods implemented

---
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/euler.h
@@ -0,0 +1,82 @@
+#ifndef EULER_H
+#define EULER_H
+#include <functional>
+#include <vector>
+#include "../../../Linear algebra/Lab1 (Direct methods for solving systems of linear equations)/src/Gauss.h"
+#include "../../../Linear algebra/Lab5 (Solving nonlinear equations)/src/FindRoots.h"
+template <typename T>
+std::vector<std::vector<T>> explicit_euler(
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
+	T t0, const std::vector<T>& u0, double t_end, double tau){
+	size_t n = f.size();
+	size_t N =std::round((t_end-t0)/tau+1);
+	std::vector<std::vector<T>> solution(n,std::vector<T>(N));
+	T t;
+	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
+	for(int i = 0; i < N-1; i++)
+	{
+		t=t0+i*tau;
+		std::vector<T> u_n(n);
+		for(int j = 0; j < n; j++)
+		{
+			u_n[j]=solution[j][i];
+		}
+		for(int j = 0; j < n; j++)
+		{
+			solution[j][i+1]=solution[j][i]+tau*f[j](t,u_n);
+		}
+	}
+	return solution;
+}
+
+template <typename T>
+std::vector<std::vector<T>> implicit_euler(
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
+	T t0, const std::vector<T>& u0, double t_end, double tau){
+	size_t n = f.size();
+	size_t N =std::round((t_end-t0)/tau+1);
+	std::vector<std::vector<T>> solution(n,std::vector<T>(1)); T t;
+	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
+	std::vector<T> y = u0;
+	auto F = [&](const std::vector<T>& y_next) -> std::vector<T> {
+		std::vector<T> residual(y_next.size());
+		for (size_t i = 0; i < y_next.size(); ++i) {
+			T f_val = f[i](t + tau, y_next);
+			residual[i] = y_next[i] - y[i] - tau * f_val;
+		}
+		return residual;
+	};
+	for(int i = 0; i < N-1; i++)
+	{
+		t=t0+i*tau;
+		std::vector<size_t> subdivisions(n);
+		std::vector<std::pair<T, T>> bounds(n);
+		for(int j = 0; j < n; j++)
+		{
+			bounds[j]=std::pair<T,T>(solution[j][i]-tau*y[j],solution[j][i]+tau*y[j]);
+			subdivisions[j]=10;
+		}
+		std:: cout << i << " " << t << " " << solution[0] << " " << solution[1] <<  std::endl;
+		while (true){
+			auto data=newtonMethod<double>(F,tau,1000,Gauss<double>, bounds, subdivisions);
+			if (data.empty())
+			{
+				for(int j = 0; j < n; j++)
+				{
+					bounds[j]=std::pair<T,T>(bounds[j].first-2*tau*solution[j][i],bounds[j].second+2*tau*solution[j][i]);
+					subdivisions[j]*=2;
+				}
+			}
+			else
+			{
+				for(int j = 0; j < n; j++)
+				{
+					solution[j].emplace_back(data[0][j]);
+				}
+				break;
+			}
+		}
+	}
+	return solution;
+}
+#endif //EULER_H
--- Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/main.cpp
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/main.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 #include <vector>
 #include "../../../Linear algebra/Lab1 (Direct methods for solving systems of linear equations)/src/matrix_operations.h"
+#include "../../../Linear algebra/Lab3 (Solving interpolation problems)/src/grid_generators.h"
 #include "runge_kutta_4th_order.h"
+#include "euler.h"
+#include <fstream>
+
+void exportData(const std::vector<std::vector<double>>& data, const std::string& filename) {
+	if (data.empty()) return;
+
+	std::ofstream outFile(filename);
+	if (!outFile) {
+		std::cerr << "Error opening file!" << std::endl;
+		return;
+	}
+
+	size_t rows = data.size();
+	size_t cols = data[0].size();
+
+	for (size_t j = 0; j < cols; ++j) {
+		for (size_t i = 0; i < rows; ++i) {
+			outFile << data[i][j] << " ";
+		}
+		outFile << "\n";
+	}
+
+	outFile.close();
+}
+
 int main()
 {
 	std::vector<std::function<double(double, const std::vector<double>&)>> funcs = {
-		[](double t, const std::vector<double>& u) { return u[1]  ; },
-		[](double t, const std::vector<double>& u) { return  - u[0] ; }
+		[](double t, const std::vector<double>& u) { return 2*u[0]+u[1]*u[1] -1;},
+		[](double t, const std::vector<double>& u) { return 6* u[0] -u[1]*u[1] +1 ; }
 	};
+	// std::vector<std::function<double(double, const std::vector<double>&)>> funcs = {
+	// 	[](double t, const std::vector<double>& u) { return -u[1];},
+	// 	[](double t, const std::vector<double>& u) { return u[0] ; }
+	// };
 
-	std::vector<double> u0 = {1.0, 0.0};
-	std::vector<std::vector<double>> result = RungeKutta4System(funcs, 0.0, u0, 10, 0.1);
+	std::vector<double> u0 = {1.0, 1.0}; double tend=0.5;
+	std::vector<std::vector<double>> result = RungeKutta4System(funcs, 0.0, u0, tend, 0.05);
+	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/runge.txt");
 	for (const auto & i : result)
 	{
 		std::cout << i << std::endl;
 	}
+	std::vector<std::vector<double>> result2 = explicit_euler(funcs, 0.0, u0, tend, 0.05);
+	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/explicit_euler.txt");
+	for (const auto & i : result2)
+	{
+		std::cout << i << std::endl;
+	}
+	std::vector<std::vector<double>> result3 = implicit_euler(funcs, 0.0, u0, tend, 0.05);
+	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/implicit_euler.txt");
+	for (const auto & i : result3)
+	{
+		std::cout << i << std::endl;
+	}
+
+
 }
