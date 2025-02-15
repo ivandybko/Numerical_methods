acad3ed32feb3515499051e6fc456f3a5be40a7a
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-15 18:09:48 +0300

    Adams-Bashforth method implemented

---
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/adams_bashforth_4th_order.h
@@ -0,0 +1,34 @@
+#ifndef ADAMS_BASHFORTH_H
+#define ADAMS_BASHFORTH_H
+#include <functional>
+#include <vector>
+#include "runge_kutta_4th_order.h"
+template<typename T>
+std::vector<std::vector<T>> adams_bashforth(
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
+	T t0, const std::vector<T>& u0, double t_end, double tau){
+	size_t n = f.size(); T t;
+	size_t N =(t_end-t0)/tau+1;
+	auto solution = runge_kutta4(f, t0, u0, t0+3*tau, tau);
+	for (auto &sol : solution){sol.resize(N);}
+	for(int i = 3; i < N-1; i++)
+	{
+		t=t0+i*tau;
+		std::vector<T> u_n(n), u_n1(n), u_n2(n), u_n3(n);
+		for(int j = 0; j < n; j++)
+		{
+			u_n[j]=solution[j][i];
+			u_n1[j]=solution[j][i-1];
+			u_n2[j]=solution[j][i-2];
+			u_n3[j]=solution[j][i-3];
+
+		}
+		for(int j = 0; j < n; j++)
+		{
+			solution[j][i + 1] = solution[j][i] + tau/24.0*(55.0* f[j](t, u_n)-59.0*f[j](t - tau, u_n1)
+			+37.0*f[j](t - 2*tau, u_n2)-9.0*f[j](t - 3* tau, u_n3));
+		}
+	}
+	return solution;
+}
+#endif //ADAMS_BASHFORTH_H
--- Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/main.cpp
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/main.cpp
@@ -6,6 +6,8 @@
 #include "euler.h"
 #include <fstream>
 
+#include "adams_bashforth_4th_order.h"
+
 void exportData(const std::vector<std::vector<double>>& data, const std::string& filename) {
 	if (data.empty()) return;
 
@@ -39,31 +41,35 @@ int main()
 	// 	[](double t, const std::vector<double>& u) { return u[0] ; }
 	// };
 
-	std::vector<double> u0 = {1.0, 1.0}; double tend=0.5;
-	std::vector<std::vector<double>> result = runge_kutta4(funcs, 0.0, u0, tend, 0.05);
+	std::vector<double> u0 = {1.0, 1.0}; double tend=1;
+	std::vector<std::vector<double>> result = runge_kutta4(funcs, 0.0, u0, tend, 0.1);
 	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/runge.txt");
 	for (const auto & i : result)
 	{
 		std::cout << i << std::endl;
 	}
-	std::vector<std::vector<double>> result2 = explicit_euler(funcs, 0.0, u0, tend, 0.05);
+	std::vector<std::vector<double>> result2 = explicit_euler(funcs, 0.0, u0, tend, 0.1);
 	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/explicit_euler.txt");
 	for (const auto & i : result2)
 	{
 		std::cout << i << std::endl;
 	}
-	std::vector<std::vector<double>> result3 = implicit_euler(funcs, 0.0, u0, tend, 0.05);
+	std::vector<std::vector<double>> result3 = implicit_euler(funcs, 0.0, u0, tend, 0.1);
 	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/implicit_euler.txt");
 	for (const auto & i : result3)
 	{
 		std::cout << i << std::endl;
 	}
-	std::vector<std::vector<double>> result4 = trapezoidal_rule_method(funcs, 0.0, u0, tend, 0.05);
+	std::vector<std::vector<double>> result4 = trapezoidal_rule_method(funcs, 0.0, u0, tend, 0.1);
 	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/symmetric.txt");
 	for (const auto & i : result4)
 	{
 		std::cout << i << std::endl;
 	}
-
-
+	std::vector<std::vector<double>> result5 = adams_bashforth(funcs, 0.0, u0, tend, 0.1);
+	exportData(result, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/data/test1/adams_bashforth.txt");
+	for (const auto & i : result5)
+	{
+		std::cout << i << std::endl;
+	}
 }
