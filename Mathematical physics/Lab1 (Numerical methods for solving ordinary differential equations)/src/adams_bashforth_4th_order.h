e98a850d986e2e242ed5dcaa0b540ef4907693b4
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-02-23 14:18:06 +0300

    Aligned to Harvard style

--- Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/adams_bashforth_4th_order.h
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/adams_bashforth_4th_order.h
@@ -5,17 +5,20 @@
 #include "runge_kutta_4th_order.h"
 template<typename T>
 std::vector<std::vector<T>> adams_bashforth(
-	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
-	T t0, const std::vector<T>& u0, T t_end, T tau, bool predictor_corrector = false){
-	size_t n = f.size(); T t;
-	size_t N =(t_end-t0)/tau+1;
-	auto solution = runge_kutta4(f, t0, u0, t0+3*tau, tau);
-	for (auto &sol : solution){sol.resize(N);}
-	for(int i = 3; i < N-1; i++)
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& func,
+	T t_start, const std::vector<T>& initial_conditions, T t_end, T tau, bool predictor_corrector = false){
+	size_t equations_number = func.size(); T t;
+	size_t points_number =(t_end-t_start)/tau+1;
+	auto solution = runge_kutta4(func, t_start, initial_conditions, t_start+3*tau, tau);
+	for (auto &sol : solution){sol.resize(points_number);}
+	for(int i = 3; i < points_number-1; i++)
 	{
-		t=t0+i*tau;
-		std::vector<T> u_n(n), u_n1(n), u_n2(n), u_n3(n);
-		for(int j = 0; j < n; j++)
+		t=t_start+i*tau;
+		std::vector<T> u_n(equations_number);
+		std::vector<T> u_n1(equations_number);
+		std::vector<T> u_n2(equations_number);
+		std::vector<T> u_n3(equations_number);
+		for(int j = 0; j < equations_number; j++)
 		{
 			u_n[j]=solution[j][i];
 			u_n1[j]=solution[j][i-1];
@@ -23,22 +26,22 @@ std::vector<std::vector<T>> adams_bashforth(
 			u_n3[j]=solution[j][i-3];
 
 		}
-		for(int j = 0; j < n; j++)
+		for(int j = 0; j < equations_number; j++)
 		{
-			solution[j][i + 1] = solution[j][i] + tau/24.0*(55.0* f[j](t, u_n)-59.0*f[j](t - tau, u_n1)
-			+37.0*f[j](t - 2*tau, u_n2)-9.0*f[j](t - 3* tau, u_n3));
+			solution[j][i + 1] = solution[j][i] + tau/24.0*(55.0* func[j](t, u_n)-59.0*func[j](t - tau, u_n1)
+			+37.0*func[j](t - 2*tau, u_n2)-9.0*func[j](t - 3* tau, u_n3));
 		}
 		if ( predictor_corrector )
 		{
-			std::vector<T> u_cor(n);
-			for(int j = 0; j < n; j++)
+			std::vector<T> u_cor(equations_number);
+			for(int j = 0; j < equations_number; j++)
 			{
 				u_cor[j]=solution[j][i+1];
 			}
-			for(int j = 0; j < n; j++)
+			for(int j = 0; j < equations_number; j++)
 			{
-				solution[j][i + 1] = solution[j][i] + tau/24.0*(9.0* f[j](t+tau, u_cor)+19.0*f[j](t, u_n)
-				-5.0*f[j](t - tau, u_n1)+f[j](t - 2* tau, u_n2));
+				solution[j][i + 1] = solution[j][i] + tau/24.0*(9.0* func[j](t+tau, u_cor)+19.0*func[j](t, u_n)
+				-5.0*func[j](t - tau, u_n1)+func[j](t - 2* tau, u_n2));
 			}
 		}
 
--- Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/euler.h
+++ Mathematical physics/Lab1 (Numerical methods for solving ordinary differential equations)/src/euler.h
@@ -7,24 +7,24 @@
 #include "../../../Linear algebra/Lab5 (Solving nonlinear equations)/src/FindRoots.h"
 template <typename T>
 std::vector<std::vector<T>> explicit_euler(
-	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
-	T t0, const std::vector<T>& u0, T t_end, T tau){
-	size_t n = f.size();
-	size_t N =std::round((t_end-t0)/tau+1);
-	std::vector<std::vector<T>> solution(n,std::vector<T>(N));
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& func,
+	T t_start, const std::vector<T>& initial_conditions, T t_end, T tau){
+	size_t equations_number = func.size();
+	size_t points_number =std::round((t_end-t_start)/tau+1);
+	std::vector<std::vector<T>> solution(equations_number,std::vector<T>(points_number));
 	T t;
-	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
-	for(int i = 0; i < N-1; i++)
+	for (size_t j = 0; j < equations_number; j++) {solution[j][0]=initial_conditions[j];}
+	for(int i = 0; i < points_number-1; i++)
 	{
-		t=t0+i*tau;
-		std::vector<T> u_n(n);
-		for(int j = 0; j < n; j++)
+		t=t_start+i*tau;
+		std::vector<T> u_n(equations_number);
+		for(int j = 0; j < equations_number; j++)
 		{
 			u_n[j]=solution[j][i];
 		}
-		for(int j = 0; j < n; j++)
+		for(int j = 0; j < equations_number; j++)
 		{
-			solution[j][i+1]=solution[j][i]+tau*f[j](t,u_n);
+			solution[j][i+1]=solution[j][i]+tau*func[j](t,u_n);
 		}
 	}
 	return solution;
@@ -32,47 +32,46 @@ std::vector<std::vector<T>> explicit_euler(
 
 template <typename T>
 std::vector<std::vector<T>> implicit_euler(
-	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
-	T t0, const std::vector<T>& u0, T t_end, T tau){
-	size_t n = f.size();
-	size_t N =std::round((t_end-t0)/tau+1);
-	std::vector<std::vector<T>> solution(n,std::vector<T>(1)); T t;
-	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
-	std::vector<T> y = u0;
-	auto F = [&](const std::vector<T>& y_next) -> std::vector<T> {
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& func,
+	T t_start, const std::vector<T>& initial_conditions, T t_end, T tau){
+	size_t equations_number = func.size();
+	size_t points_number =std::round((t_end-t_start)/tau+1);
+	const int max_iterations = 1000;
+	const int boundary_expansion = 10;
+	std::vector<std::vector<T>> solution(equations_number,std::vector<T>(1)); T t;
+	for (size_t j = 0; j < equations_number; j++) {solution[j][0]=initial_conditions[j];}
+	std::vector<T> y = initial_conditions;
+	auto residual_function = [&](const std::vector<T>& y_next) -> std::vector<T> {
 		std::vector<T> residual(y_next.size());
 		for (size_t i = 0; i < y_next.size(); ++i) {
-			T f_val = f[i](t + tau, y_next);
+			T f_val = func[i](t + tau, y_next);
 			residual[i] = y_next[i] - y[i] - tau * f_val;
 		}
 		return residual;
 	};
-	for(int i = 0; i < N-1; i++)
+	for(int i = 0; i < points_number-1; i++)
 	{
-		t=t0+i*tau;
-		std::vector<size_t> subdivisions(n);
-		std::vector<std::pair<T, T>> bounds(n);
-		// std::cout << i << std::endl;
-		for(int j = 0; j < n; j++)
+		t=t_start+i*tau;
+		std::vector<size_t> subdivisions(equations_number);
+		std::vector<std::pair<T, T>> bounds(equations_number);
+		for(int j = 0; j < equations_number; j++)
 		{
-			bounds[j]=std::pair<T,T>(solution[j][i]-10*tau*(abs(y[j]+1)),solution[j][i]+10*tau*(abs(y[j]+1)));
-			subdivisions[j]=10;
+			bounds[j]=std::pair<T,T>(solution[j][i]-boundary_expansion*tau*(abs(y[j]+1)),solution[j][i]+boundary_expansion*tau*(abs(y[j]+1)));
+			subdivisions[j]=boundary_expansion;
 		}
 		while (true){
-			auto data=newtonMethod<T>(F,tau,1000, Gauss<double>, bounds, subdivisions);
-			// std::cout << bounds << std::endl;
-			// std::cout << data << std::endl;
+			auto data=newtonMethod<T>(residual_function,tau, max_iterations, Gauss<double>, bounds, subdivisions);
 			if (data.empty())
 			{
-				for(int j = 0; j < n; j++)
+				for(int j = 0; j < equations_number; j++)
 				{
-					bounds[j]=std::pair<T,T>(bounds[j].first-10*tau*(abs(solution[j][i])+1),bounds[j].second+10*tau*(abs(solution[j][i])+1));
+					bounds[j]=std::pair<T,T>(bounds[j].first-boundary_expansion*tau*(abs(solution[j][i])+1),bounds[j].second+boundary_expansion*tau*(abs(solution[j][i])+1));
 					subdivisions[j]*=2;
 				}
 			}
 			else
 			{
-				for(int j = 0; j < n; j++)
+				for(int j = 0; j < equations_number; j++)
 				{
 					solution[j].emplace_back(data[0][j]);
 					y[j]=solution[j][i+1];
@@ -86,52 +85,48 @@ std::vector<std::vector<T>> implicit_euler(
 
 template <typename T>
 std::vector<std::vector<T>> trapezoidal_rule_method(
-	const std::vector<std::function<T(T, const std::vector<T>&)>>& f,
-	T t0, const std::vector<T>& u0, T t_end, T tau){
-	size_t n = f.size();
-	size_t N =std::round((t_end-t0)/tau+1);
-	std::vector<std::vector<T>> solution(n,std::vector<T>(1)); T t;
-	for (size_t j = 0; j < n; j++) {solution[j][0]=u0[j];}
-	std::vector<T> y = u0;
-	auto F = [&](const std::vector<T>& y_next) -> std::vector<T> {
+	const std::vector<std::function<T(T, const std::vector<T>&)>>& func,
+	T t_start, const std::vector<T>& initial_conditions, T t_end, T tau){
+	const int max_iterations = 1000;
+	const int boundary_expansion = 10;
+	int const grid_refinement = 10;
+	size_t equations_number = func.size();
+	size_t points_number =std::round((t_end-t_start)/tau+1);
+	std::vector<std::vector<T>> solution(equations_number,std::vector<T>(1)); T t;
+	for (size_t j = 0; j < equations_number; j++) {solution[j][0]=initial_conditions[j];}
+	std::vector<T> y = initial_conditions;
+	auto residual_function = [&](const std::vector<T>& y_next) -> std::vector<T> {
 		std::vector<T> residual(y_next.size());
 		for (size_t i = 0; i < y_next.size(); ++i) {
-			T f_val = f[i](t + tau, y_next);
-			T f_val_cur = f[i](t, y);
+			T f_val = func[i](t + tau, y_next);
+			T f_val_cur = func[i](t, y);
 			residual[i] = y_next[i] - y[i] - tau/2 * f_val - tau/2 * f_val_cur;
 		}
-		// if (compute2Norm(residual) < tau)
-		// {
-		// 	std::cout << compute2Norm(residual) << std::endl;
-		// }
 		return residual;
 	};
-	for(int i = 0; i < N-1; i++)
+	for(int i = 0; i < points_number-1; i++)
 	{
-		t=t0+i*tau;
-		std::vector<size_t> subdivisions(n);
-		std::vector<std::pair<T, T>> bounds(n);
-		for(int j = 0; j < n; j++)
+		t=t_start+i*tau;
+		std::vector<size_t> subdivisions(equations_number);
+		std::vector<std::pair<T, T>> bounds(equations_number);
+		for(int j = 0; j < equations_number; j++)
 		{
-			bounds[j]=std::pair<T,T>(solution[j][i]-10*tau*(abs(y[j]+1)),solution[j][i]+10*tau*(abs(y[j]+1)));
-			subdivisions[j]=10;
+			bounds[j]=std::pair<T,T>(solution[j][i]-boundary_expansion*tau*(abs(y[j]+1)),solution[j][i]+boundary_expansion*tau*(abs(y[j]+1)));
+			subdivisions[j]=grid_refinement;
 		}
-		// std::cout<< i << std::endl;
 		while (true){
-			auto data=newtonMethod<T>(F,tau,1000,Gauss<double>, bounds, subdivisions);
-			// std::cout << bounds << std::endl;
-			// std::cout << data << std::endl;
+			auto data=newtonMethod<T>(residual_function,tau,max_iterations,Gauss<double>, bounds, subdivisions);
 			if (data.empty())
 			{
-				for(int j = 0; j < n; j++)
+				for(int j = 0; j < equations_number; j++)
 				{
-					bounds[j]=std::pair<T,T>(bounds[j].first-10*tau*(abs(solution[j][i])+1),bounds[j].second+10*tau*(abs(solution[j][i])+1));
+					bounds[j]=std::pair<T,T>(bounds[j].first-boundary_expansion*tau*(abs(solution[j][i])+1),bounds[j].second+boundary_expansion*tau*(abs(solution[j][i])+1));
 					subdivisions[j]*=2;
 				}
 			}
 			else
 			{
-				for(int j = 0; j < n; j++)
+				for(int j = 0; j < equations_number; j++)
 				{
 					solution[j].emplace_back(data[0][j]);
 					y[j]=solution[j][i+1];
