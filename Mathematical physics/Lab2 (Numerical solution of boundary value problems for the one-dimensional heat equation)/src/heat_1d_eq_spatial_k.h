9431f1a6a5f7d72d82a8ab6b3084d5c99b15d4f7
Ivan Dybko <dyybkoie@student.bmstu.ru>
2025-04-09 18:47:30 +0300

    Lab finished

--- Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/src/heat_1d_eq_spatial_k.h
+++ Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/src/heat_1d_eq_spatial_k.h
@@ -1,12 +1,8 @@
 #ifndef HEAT_1D_EQ_SPATIAL_K_H
 #define HEAT_1D_EQ_SPATIAL_K_H
 #include <functional>
-#include <variant>
 #include "../../../Linear algebra/Lab1 (Direct methods for solving systems of linear equations)/src/matrix_operations.h"
 #include "../../../Linear algebra/Lab1 (Direct methods for solving systems of linear equations)/src/TridiagonalMatrixAlgorithm.h"
-
-// template <typename T>
-// using cond_type = std::variant<std::function<T(T)>, std::function<T(T, T)>, std::function<T(T, T, T)>>;
 
 template <typename T>
 std::vector<std::vector<T>> heat_eq_spatial(T rho, T c, T k1, T k2, T x1, T x2, T Q, T t0, T L, T u0,  std::function<T(std::vector<T>)> K, std::function<T(std::vector<T>)> initial_cond,  std::function<T(std::vector<T>)> left_cond, bool stream_left, std::function<T(std::vector<T>)> right_cond, bool stream_right, T tau, T h, T sigma)
@@ -14,70 +10,116 @@ std::vector<std::vector<T>> heat_eq_spatial(T rho, T c, T k1, T k2, T x1, T x2, 
 	size_t num_points_in_space = std::round(L / h)+1;
 	size_t num_points_over_time = std::round(t0 / tau)+1;
 	std::vector<std::vector<T>> solution(1, std::vector<T>(num_points_in_space));
-	std::vector<T> A(num_points_in_space-3);
-	std::vector<T> a(num_points_in_space);
-	std::vector<T> B(num_points_in_space-3);
-	std::vector<T> C(num_points_in_space-2);
-	std::vector<T> F(num_points_in_space-2);
+	std::vector<T> A(num_points_in_space-1);
+	std::vector<T> a(num_points_in_space+1);
+	std::vector<T> B(num_points_in_space-1);
+	std::vector<T> C(num_points_in_space);
+	std::vector<T> F(num_points_in_space);
 	solution[0][0]=initial_cond({0 , L, u0});;
-	for (size_t i = 1; i < num_points_in_space; i++){
+	for (size_t i = 0; i < num_points_in_space; i++){
 		solution[0][i] = initial_cond({i*h , L, u0});
-		// a[i] = 2.0 * K({(i - 1)*h, L, x1, x2, k1, k2, L}) * K({i*h,L, x1, x2, k1, k2}) / (K({(i - 1)*h, L, x1, x2, k1, k2, L}) + K({i*h, L, x1, x2, k1, k2}));
-		a[i] = std::sqrt(K({(i - 1)*h, L, x1, x2, k1, k2, L}) * K({i*h, L, x1, x2, k1, k2, L}));
-		// a[i] = std::sqrt(K((i - 1)*h, x1, x2, k1, k2, L) * K(i*h, x1, x2, k1, k2, L));
+		a[i] = 2.0 * K({(i - 1)*h, L, x1, x2, k1, k2, L}) * K({i*h,L, x1, x2, k1, k2}) / (K({(i - 1)*h, L, x1, x2, k1, k2, L}) + K({i*h, L, x1, x2, k1, k2}));
 	}
 	for (size_t j = 1; j < num_points_over_time; j++)
 	{
-		for (size_t i = 1; i <= num_points_in_space-3; i++)
+		for (size_t i = 1; i < num_points_in_space-1; i++)
 		{
-			A[i-1] = (sigma/ h) * a[i+1];
-			B[i-1] = (sigma/ h) * a[i+1];
+			A[i-1] = (sigma/ h) * a[i];
+			B[i] = (sigma/ h) * a[i+1];
 		}
-		for (size_t i = 1; i <= num_points_in_space-2; i++)
+		for (size_t i = 1; i < num_points_in_space-1; i++)
 		{
-			C[i-1] = - (sigma/ h * a[i] + sigma/ h * a[i+1] +  c*rho*h/tau);
-			F[i-1] = - (c*rho*h/tau*solution[j-1][i] + (1-sigma)*(a[i + 1] * (solution[j-1][i + 1] - solution[j-1][i]) / h - a[i] * (solution[j-1][i] - solution[j-1][i-1]) / h));
+			C[i] = - ((sigma/ h) * a[i] + (sigma/ h) * a[i+1] +  c*rho*h/tau);
+			F[i] = - (c*rho*h/tau*solution[j-1][i] + (1-sigma)*(a[i + 1] * (solution[j-1][i + 1] - solution[j-1][i]) / h - a[i] * (solution[j-1][i] - solution[j-1][i-1]) / h));
 		}
-		std::vector<T> internal_points = TridiagonalMatrixAlgorithm(A, C, B, F);
-		std::vector<T> temp{};
-		// if (stream_left)
-		// {
-		// 	double P_tj1 = left_cond({tau*j, Q, t0});
-		// 	double P_tj = left_cond({tau*(j-1), Q, t0});
-		// 	double chi = (sigma * a[1] / h) / (c * rho * h / (2 * tau) + sigma * a[1] / h);
-		// 	double numerator = c * rho * solution[j-1][0] * h / (2 * tau) + sigma * P_tj1 + (1 - sigma) * (P_tj - (solution[j-1][1] - solution[j-1][0]) / h);
-		// 	double denominator = c * rho * h / (2 * tau) + sigma * a[1] / h;
-		// 	double mu = numerator / denominator;
-		// 	temp.push_back(chi*internal_points[0]+mu);
-		// }
 		if (stream_left)
 		{
-			T P_tj1 = left_cond({tau*j, Q, t0});
-			T P_tj = left_cond({tau*(j-1), Q, t0});
-			T left= ((h*h+ 2*(-1+sigma)*tau*a[1])*solution[j-1][0]+2*tau*(h*(P_tj-P_tj*sigma+P_tj1* sigma)-(-1+sigma)*a[1]*solution[j-1][1]*sigma*a[1]*internal_points[0]))/(h*h+2*sigma*tau * a[1]);
-			temp.push_back(left);
+			C[0] = c * rho * h / 2 + tau * sigma * K({h/2,L, x1, x2, k1, k2}) / h;
+			B[0] = -tau * sigma * K({h/2,L, x1, x2, k1, k2}) / h;
+			F[0] = c * rho * h / 2 * solution[j-1][0]  + tau * (1 - sigma) * (K({h/2,L, x1, x2, k1, k2}) * (solution[j-1][1]  - solution[j-1][0] ) / h - left_cond({tau*j, Q, t0})) + tau * sigma * left_cond({tau*(j+1), Q, t0});
 		}
-
-		// solution[j][0] = left_cond(j*tau);
 		else
 		{
-			temp.push_back(left_cond({j * tau}));
+			B[0]=0;
+			C[0]=1;
+			F[0]=left_cond({tau*j, Q, t0});
 		}
-		temp.insert(temp.end(), internal_points.begin(), internal_points.end());
 		if (stream_right)
 		{
-			T P_tj1 = right_cond({tau*j, Q, t0});
-			T P_tj = right_cond({tau*(j-1), Q, t0});
-			T chi = (sigma * a[num_points_in_space-1] / h) / (c * rho * h / (2 * tau) + sigma * a[num_points_in_space-1] / h);
-			T numerator = c * rho * solution[j-1][num_points_in_space-1] * h / (2 * tau) + sigma * P_tj1 + (1 - sigma) * (P_tj - (solution[j-1][num_points_in_space-1] - solution[j-1][num_points_in_space-2]) / h);
-			T denominator = c * rho * h / (2 * tau) + sigma * a[num_points_in_space-1] / h;
-			T mu = numerator / denominator;
-			temp.push_back(chi*temp[num_points_in_space-2]+mu);
+			A[num_points_in_space-2] = -(sigma * a[num_points_in_space-1] / h) / ((c * rho * h) / (2 * tau) + sigma * a[num_points_in_space-1] / h);
+			C[num_points_in_space-1] = 1;
+			F[num_points_in_space-1] = ((c * rho * solution[j-1][num_points_in_space-1] * h) / (2 * tau) + sigma * right_cond({tau*(j+1), Q, t0}) + (1 - sigma) * (right_cond({tau*j, Q, t0}) - a[num_points_in_space-1] * (solution[j-1][num_points_in_space-1] - solution[j-1][num_points_in_space-2]) / h)) /
+				(c * rho * h / (2 * tau) + sigma * a[num_points_in_space-1] / h);
 		}
 		else{
-			temp.push_back(right_cond({j * tau}));
+			A[num_points_in_space-2]=0;
+			C[num_points_in_space-1]=1;
+			F[num_points_in_space-1]=right_cond({tau*j, Q, t0});
 		}
-		solution.emplace_back(std::move(temp));
+		std::vector<T> internal_points = TridiagonalMatrixAlgorithm(A, C, B, F);
+ 		solution.emplace_back(std::move(internal_points));
+	}
+	return solution;
+}
+
+template <typename T>
+std::vector<std::vector<T>> heat_eq_nonlinear(T rho, T c, T alpha, T beta, T gamma, T t0, T L, T u0,  std::function<T(std::vector<T>)> K, std::function<T(std::vector<T>)> initial_cond,  std::function<T(std::vector<T>)> left_cond, bool stream_left, std::function<T(std::vector<T>)> right_cond, bool stream_right, T tau, T h, T max_iter=1000, T epsilon=1e-6)
+{
+	size_t num_points_in_space = std::round(L / h);
+	size_t num_points_over_time = std::round(t0 / tau);
+	std::vector<std::vector<T>> solution(1, std::vector<T>(num_points_in_space+1));
+	std::vector<T> A(num_points_in_space);
+	std::vector<T> a(num_points_in_space+1);
+	std::vector<T> B(num_points_in_space+1);
+	std::vector<T> C(num_points_in_space);
+	std::vector<T> F(num_points_in_space+1);
+	solution[0][0]=initial_cond({0 , L, u0});;
+	std::vector<T> u_prev(num_points_in_space + 1, 0.0), u_current(num_points_in_space + 1);
+	for (size_t i = 1; i < num_points_in_space; i++){
+		solution[0][i] = initial_cond({i*h , L, u0});
+	}
+	for (int n = 1; n < num_points_over_time; ++n)
+	{
+		T t = n * tau;
+		for (int iter = 0; iter < max_iter; ++iter)
+		{
+			u_prev = u_current;
+			for (int i = 1; i < num_points_in_space; ++i) {
+				T Ki = K({u_prev[i], alpha, beta, gamma});
+				T Ki1 = K({u_prev[i + 1], alpha, beta, gamma});
+				T Ki_1 = K({u_prev[i - 1], alpha, beta, gamma});
+				T a_i = 0.5 * (Ki + Ki_1);
+				T a_i1 = 0.5 * (Ki1 + Ki);
+				A[i-1] = a_i / h;
+				C[i] = a_i1 / h;
+				B[i] = -A[i-1] - C[i] - c * rho *h / tau;
+				F[i] = -c * rho * h * u_prev[i] / tau;
+				if (i * h >= c * t)
+				{
+					A[i-1] = 0;
+					B[i] = 1;
+					C[i] = 0;
+					F[i] = right_cond({});
+				}
+			}
+			B[0] = 1;
+			C[0] = 0;
+			F[0] = left_cond({t, beta, gamma, c});
+			A[num_points_in_space-1] = 0;
+			B[num_points_in_space] = 1;
+			F[num_points_in_space] = right_cond({});
+			u_current = TridiagonalMatrixAlgorithm(A, B, C, F);
+			double max_diff = 0.0;
+			for (int i = 0; i <= num_points_in_space; ++i) {
+				max_diff = std::max(max_diff, abs(u_current[i] - u_prev[i]));
+			}
+			if (max_diff < epsilon) break;
+		}
+		for (int i = 0; i <= num_points_in_space; i++)
+		{
+			u_prev[i] = u_current[i];
+		}
+		solution.emplace_back(u_current);
 	}
 	return solution;
 }
--- Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/src/main.cpp
+++ Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/src/main.cpp
@@ -57,99 +57,122 @@ std::vector<double> error(double h, double tau, double sigma, double L, double t
 	}
 	return max;
 }
+std::function<double(std::vector<double>)> u0 = [](std::vector<double> data){return 0.0;};
+std::function<double(std::vector<double>)> u2 = [](std::vector<double> data){return 10.0;};
+std::function<double(std::vector<double>)> const P = [](std::vector<double> data){return 0.0;};
+std::function<double(std::vector<double>)> const P1 = [](std::vector<double> data) {
+	double t = data[0];
+	double Q = data[1];
+	double t0 = data[2];
+	if (0 <= t && t <= t0) {
+		return Q;
+	}
+	return 0.0;
+};
+std::function<double(std::vector<double>)> const P4 = [](std::vector<double> data) {
+	double t = data[0];
+	double Q = data[1];
+	double t0 = data[2];
+	if (0 <= t && t <= 0.5 * t0) {
+		return 2 * Q * t;
+	}
+	if (0.5 * t0 < t && t < t0) {
+			return 2 * Q * (t0 - t);
+	}
+	return 0.0;
+};
+// std::function<double(std::vector<double>)> const P1 = [](std::vector<double> data) {
+// 	return 1.0;
+// };
+std::function<double(std::vector<double>)> const initial_cond = [](std::vector<double> data) {
+	double x = data[0];
+	double l = data[1];
+	double u0 = data[2];
+	return u0 + x*(l-x);
+};
+std::function<double(std::vector<double>)> const initial_cond0 = [](std::vector<double> data) {
+	return data[0];
+	// return 0.0;
+};
+std::function<double(std::vector<double>)> const initial_cond_L = [](std::vector<double> data) {
+	double x = data[0];
+	double L = data[1];
+	return std::pow(1.0 + x / L, -0.5) *
+	   std::sin((M_PI / std::log(2.0)) * std::log(1.0 + x / L));
+};
+std::function<double(std::vector<double>)> const K_L = [](std::vector<double> data) {
+	double x = data[0];
+	double L = data[1];
+	return std::pow(1.0 + x / L, 2);
+};
+std::function<double(std::vector<double>)> const K = [](std::vector<double> data) {
+	double x = data[0];
+	double x1 = data[1];
+	double x2 = data[2];
+	double k1 = data[3];
+	double k2 = data[4];
+	double L = data[5];
+	if (0 <= x && x <= x1) {
+		return k1;
+	}
+	if (x1 < x && x < x2) {
+		return k1 * (x - x2) / (x1 - x2) + k2 * (x - x1) / (x2 - x1);
+	}
+	if (x2 <= x && x <= L) {
+		return k2;
+	}
+	std::cerr << "Function K(x) out of range!" << std::endl;
+	return 0.0;
+};
+std::function<double(std::vector<double>)> const K_nonlinear = [](std::vector<double> data) {
+	double u = data[0];
+	double alpha = data[1];
+	double beta = data[2];
+	double gamma = data[3];
+	return alpha + beta*std::pow(u, gamma);
+};
+
+std::function<double(std::vector<double>)> const left_nonlinear = [](std::vector<double> data)
+{
+	double t = data[0];
+	double beta = data[1];
+	double gamma = data[2];
+	double c = data[3];
+	return pow(((gamma * c * c) / beta), 1.0 / gamma) * pow(t, 1.0 / gamma);
+};
+std::function<double(std::vector<double>)> const right_nonlinear = [](std::vector<double> data)
+{
+	return 0;
+};
+std::function<double(std::vector<double>)> const K1 = [](std::vector<double> data) {
+	return 100.0;
+};
 
 int main()
 {
-	std::function<double(std::vector<double>)> u0 = [](std::vector<double> data){return 0.0;};
-	std::function<double(std::vector<double>)> const P = [](std::vector<double> data){return 1;};
-	std::function<double(std::vector<double>)> const P1 = [](std::vector<double> data) {
-		double t = data[0];
-		double Q = data[1];
-		double t0 = data[2];
-		if (0 <= t && t <= t0) {
-			return Q;
-		}
-		return 0.0;
-	};
-	std::function<double(std::vector<double>)> const P4 = [](std::vector<double> data) {
-		double t = data[0];
-		double Q = data[1];
-		double t0 = data[2];
-		if (0 <= t && t <= 0.5 * t0) {
-			return 2 * Q * t;
-		}
-		if (0.5 * t0 < t && t < t0) {
-				return 2 * Q * (t0 - t);
-		}
-		return 0.0;
-	};
-	// std::function<double(std::vector<double>)> const P1 = [](std::vector<double> data) {
-	// 	return 1.0;
-	// };
-	std::function<double(std::vector<double>)> const initial_cond = [](std::vector<double> data) {
-		double x = data[0];
-		double l = data[1];
-		double u0 = data[2];
-		return u0 + x*(l-x);
-	};
-	std::function<double(std::vector<double>)> const initial_cond0 = [](std::vector<double> data) {
-		return 0;
-	};
-	std::function<double(std::vector<double>)> const initial_cond_L = [](std::vector<double> data) {
-		double x = data[0];
-		double L = data[1];
-		return std::pow(1.0 + x / L, -0.5) *
-		   std::sin((M_PI / std::log(2.0)) * std::log(1.0 + x / L));
-	};
-	std::function<double(std::vector<double>)> const K_L = [](std::vector<double> data) {
-		double x = data[0];
-		double L = data[1];
-		return std::pow(1.0 + x / L, 2);
-	};
-	std::function<double(std::vector<double>)> const K = [](std::vector<double> data) {
-		double x = data[0];
-		double x1 = data[1];
-		double x2 = data[2];
-		double k1 = data[3];
-		double k2 = data[4];
-		double L = data[5];
-		if (0 <= x && x <= x1) {
-			return k1;
-		}
-		if (x1 < x && x < x2) {
-			return k1 * (x - x2) / (x1 - x2) + k2 * (x - x1) / (x2 - x1);
-		}
-		if (x2 <= x && x <= L) {
-			return k2;
-		}
-		std::cerr << "Function K(x) out of range!" << std::endl;
-		return 0.0;
-	};
-	std::function<double(std::vector<double>)> const K1 = [](std::vector<double> data) {
-		return 500;
-	};
 
-	double tau=0.001;
-	double h=0.1;
-	double t0{10.0}, L{10.0};
 
-	// auto sol0 = heat_eq_spatial<double>(1, 1, 1.0, 0.1, 0.5, 2.0/3.0, 10.0, t0, L, 0.0, K_L,initial_cond_L, u0,false,  u0, false, tau, h, 1.0);
+	double tau=0.01;
+	double h=0.2;
+	double t0{1.0}, L{10.0};
+
+	auto sol0 = heat_eq_spatial<double>(1, 1, 1.0, 0.1, 0.5, 2.0/3.0, 10.0, t0, L, 0.0, K1,initial_cond0, P,true,  P, true, tau, h, 1.0);
 	auto grid = generate_uniform_grid<double>(0, t0, t0/tau+1, 0, L, L/h+1);
-	// exportData(grid, sol0, 10, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test0.txt");
-	auto sol5 = heat_eq_spatial<double>(1, 1, 1.0, 0.1, 0.5, 2.0/3.0, 10.0, t0, L, 0.0, K_L,initial_cond_L, u0,false,  u0, false, tau, h, 0.5);
-	exportData(grid, sol5, 10, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test5.txt");
+	exportData(grid, sol0, 0, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test0.txt");
+	// auto sol5 = heat_eq_spatial<double>(1, 1, 1.0, 0.1, 0.5, 2.0/3.0, 10.0, t0, L, 0.0, K_L,initial_cond_L, u0,false,  u0, false, tau, h, 0.5);
+	// exportData(grid, sol5, 10, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test5.txt");
 	// auto sol1 = heat_eq_spatial<double>(1, 1, 1.0, 0.1, 0.5, 2.0/3.0, 10.0, t0, L, 0.0, K_L,initial_cond_L, u0,false,  u0, false, tau, h, 1.0);
 	// exportData(grid, sol1, 10, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test1.txt");
 
-	// auto sol8 = heat_eq_spatial<double>(0.25, 2, 1.5, 0.1, 0.4, 0.5, 10.0, t0, L, 0.2, K,u0, u0,false,  P4, true, tau, h, 1.0);
+	// auto sol8 = heat_eq_spatial<double>(1.25, 4, 1.5, 0.1, 0.4, 0.5, 10.0, t0, L, 0.1, K,u0, u0,false,  P4, true, tau, h, 1.0);
 	// auto grid = generate_uniform_grid<double>(0, t0, t0/tau+1, 0, L, L/h+1);
 	// exportData(grid, sol8, 10, "/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/test21.txt");
-	// for (auto &x : sol)
-	// {
-	// 	std::cout << x << std::endl;
-	// }
+	for (auto &x : sol0)
+	{
+		std::cout << x << std::endl;
+	}
 	std::ofstream outFile("/Users/ivandybko/Projects/Numerical_methods/Mathematical physics/Lab2 (Numerical solution of boundary value problems for the one-dimensional heat equation)/data/err.txt");
-	for (int i=2; i<40 ; i++)
+	for (int i=2; i<4 ; i++)
 	{
 		auto err = error(h, tau, 0.5, L, t0, u0, K_L, initial_cond_L);
 		for (auto elem : err)
@@ -158,8 +181,9 @@ int main()
 		}
 		outFile << '\n';
 		std::cout << h << '\n';
-		h = 1.0/(i*2.0);
+		// h = 1.0/(i*2.0);
+		h/=2;
+		tau/=2;
 	}
-	outFile.close();
 	return 0;
 }
